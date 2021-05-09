8d79b7c9e897edc1cb4fcf8d73b27d1835e78be3
Anastasia <anastaisafedotova@gmail.com>
2021-05-10 02:51:26 +0300

    refactor: add error messadge

--- caesarCipherCLItool/src/check/files.js
+++ caesarCipherCLItool/src/check/files.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = function checkFile(filePath) {
   fs.access(filePath, (err) => {
     if (err) {
-      process.stderr.write(`${filePath}: the file is missing or there is no access to the file`);
+      process.stderr.write(`Error: ${filePath} \n the file is missing or there is no access to the file`);
       process.exit();
     }
   });
