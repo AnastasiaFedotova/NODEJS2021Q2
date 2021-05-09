4a9e96f01ee0ff5bec8e452175f1b62add55d74f
Anastasia <anastaisafedotova@gmail.com>
2021-05-09 23:33:15 +0300

    feat: add basic functionality

---
+++ caesarCipherCLItool/src/app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const cipher = require('./cipher/cipher');
+const options = require('./options/options');
+
+function checkFile(filePath) {
+  fs.access(filePath, (err) => {
+    if (err) process.stderr.write(`${filePath}: the file is missing or there is no access to the file`);
+  });
+}
+
+if (options.input) checkFile(options.input);
+if (options.output) checkFile(options.output);
+
+if (!options.input) {
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    const result = cipher.getCipher(chunk.toString(), +options.shift);
+
+    !options.output ? process.stdout.write(`result: ${result}`)
+      : fs.createWriteStream(options.output, { flags: 'a' }).write(result);
+
+    process.stdin.resume();
+  });
+} else if (!options.output) {
+  const readableStream = fs.createReadStream(options.input, 'utf8');
+  readableStream.on('data', (chunk) => {
+    process.stdout.write(cipher.getCipher(chunk.toString(), +options.shift));
+  });
+} else {
+  const readableStream = fs.createReadStream(options.input, 'utf8');
+  const writeableStream = fs.createWriteStream(options.output);
+
+  readableStream.pipe(writeableStream);
+}
